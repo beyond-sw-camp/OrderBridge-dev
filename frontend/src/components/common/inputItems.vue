cda0499bdd6585c4b8d9063d4e2645fdaa2312a0
dispear <kkm7hjh@naver.com>
2024-12-04 11:51:17 +0900

    feat: 프론트엔드 입력폼 초안 진행

---
+++ frontend/src/components/common/inputItems.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <hr>
+  <h3>물품 등록</h3>
+  <div class="input-button-div">
+    <b-button class="input-button" size="lg" variant="outline-dark">+</b-button>
+  </div>
+  <div>
+    <b-button pill variant="primary">확인</b-button>
+    <b-button pill >목록</b-button>
+  </div>
+</template>
+
+<style scoped>
+.input-button-div{
+  margin-bottom: 50px;
+}
+
+.input-button {
+  width: 100%;
+}
+</style>
---
+++ frontend/src/views/quotation/InputView.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import InputForm from "@/components/common/InputForm.vue";
+import InputItems from "@/components/common/inputItems.vue";
+</script>
+
+<template>
+  <div class="main">
+    <h1>입력폼 초안 페이지</h1>
+    <div class="input-form-div">
+      <InputForm />
+    </div>
+    <div>
+      <InputItems />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input-form-div {
+  display: flex;
+  max-width: 600px;
+}
+</style>
