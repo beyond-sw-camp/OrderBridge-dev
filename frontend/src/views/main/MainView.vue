832e212bfc566efc548d8b7dd0ef57046d5e2ce1
AYeong-Jeon <ay98923@naver.com>
2024-12-04 10:05:29 +0900

    feat: 버튼 수정

--- frontend/src/views/main/MainView.vue
+++ frontend/src/views/main/MainView.vue
@@ -73,17 +73,49 @@ function tryDemo() {
 
 .hero-description {
   font-size: 1.2rem;
-  color: #555;
+  color: #000000;
   margin-bottom: 2rem;
   font-family: 'Alkalami', serif;
 }
 
-.hero-button {
+button {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  outline: none;
+  border: 0;
+  vertical-align: middle;
+  text-decoration: none;
+  font-size: inherit;
+  font-family: inherit;
+}
+
+button.hero-button {
   font-family: 'Abhaya Libre', serif;
-  background-color: #D7E5FF;
-  height: 50px;
-  width: 150px;
-  border-radius: 20px;
+  font-size: 1.1rem;
+  background-color: #d8e9ff;
+  border: 2px solid black;
+  text-transform: uppercase;
+  padding: 1em 2em;
+  border-radius: 15px;
+  transform-style: preserve-3d;
+  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), background 150ms cubic-bezier(0, 0, 0.58, 1);
+}
+button.hero-button::before {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin-left: 10px;
+  background-color: #fdf4e4;;
+  border-radius: inherit;
+  box-shadow: 0 0 0 2px black;
+  transform: translate3d(0, 0.75em, -1em);
+  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
 }
 
 .hero-button:hover {
