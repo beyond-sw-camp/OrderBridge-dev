4aaca65da65df4e702a06b2e012e249f94cae6fb
beanteacher <dh65432@naver.com>
2024-12-05 17:51:43 +0900

    feat: 생산입고 구현 10/100

---
+++ SCM/backend/src/main/java/error/pirate/backend/productionReceiving/command/application/controller/ProductionReceivingController.java
@@ -0,0 +1,32 @@
+package error.pirate.backend.productionReceiving.command.application.controller;
+
+import error.pirate.backend.productionReceiving.command.application.dto.ProductionReceivingCreateRequest;
+import error.pirate.backend.productionReceiving.command.application.service.ProductionReceivingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/v1/productionReceiving/")
+@Tag(name = "생산입고 API", description = "생산입고 API")
+public class ProductionReceivingController {
+
+    private final ProductionReceivingService productionReceivingService;
+
+    @PostMapping
+    @Operation(summary = "생산입고 등록")
+    public ResponseEntity<Void> createProductionReceiving(@RequestBody ProductionReceivingCreateRequest request) {
+        productionReceivingService.createProductionReceiving(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ SCM/backend/src/main/java/error/pirate/backend/productionReceiving/command/application/service/ProductionReceivingService.java
@@ -0,0 +1,14 @@
+package error.pirate.backend.productionReceiving.command.application.service;
+
+import error.pirate.backend.productionReceiving.command.application.dto.ProductionReceivingCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductionReceivingService {
+
+    public void createProductionReceiving(ProductionReceivingCreateRequest request) {
+
+    }
+}
