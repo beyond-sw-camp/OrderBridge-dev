1dfda79e3cfa4d89d5c5424c159917177324dfa5
dispear <kkm7hjh@naver.com>
2024-12-06 17:05:55 +0900

    feat: 백엔드 출하지시서 등록 진행

---
+++ SCM/backend/src/main/java/error/pirate/backend/shippingInstruction/command/application/service/ShippingInstructionApplicationService.java
@@ -0,0 +1,7 @@
+package error.pirate.backend.shippingInstruction.command.application.service;
+
+public class ShippingInstructionApplicationService {
+
+    public void createShippingInstruction(ShippingInstructionRequest evaluationRequest) {
+    }
+}
--- SCM/backend/src/main/java/error/pirate/backend/shippingInstruction/command/domain/aggregate/entity/ShippingInstructionStatus.java
+++ SCM/backend/src/main/java/error/pirate/backend/shippingInstruction/command/domain/aggregate/entity/ShippingInstructionStatus.java
@@ -1,4 +1,18 @@
 package error.pirate.backend.shippingInstruction.command.domain.aggregate.entity;
 
 public enum ShippingInstructionStatus {
+
+    BEFOREAPPROVAL("결재 전"),
+    AFTERAPPROVAL("결재 후"),
+    DELETE("삭제");
+
+    private final String label;
+
+    ShippingInstructionStatus(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
 }
