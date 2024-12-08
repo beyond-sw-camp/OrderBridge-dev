0c9a39c15c6c756f36a61af94959b01203fd1fc3
beanteacher <dh65432@naver.com>
2024-12-08 14:38:37 +0900

    feat: 생산 입고 등록 구현 완료

--- SCM/backend/src/main/java/error/pirate/backend/productionReceiving/command/application/dto/ProductionReceivingCreateRequest.java
+++ SCM/backend/src/main/java/error/pirate/backend/productionReceiving/command/application/dto/ProductionReceivingCreateRequest.java
@@ -1,13 +1,24 @@
 package error.pirate.backend.productionReceiving.command.application.dto;
 
+import error.pirate.backend.productionReceiving.command.domain.aggregate.entity.ProductionReceivingStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductionReceivingCreateRequest {
+    private Long productionWarehouseSeq; // 생산 공장 번호
+    private Long storeWarehouseSeq; // 입고 창고 번호
+    private Long userSeq; // 생산 입고 담당자 번호
+    private Long workOrderSeq; // 작업지시서 번호
+    private String productionReceivingName; // 생산입고명
+    private Integer productionReceivingExtendedPrice; // 생산입고 총액
+    private String productionReceivingNote; // 생산입고 비고
+    private List<ProductionReceivingItemDTO> productionReceivingItemList;
 }
--- SCM/backend/src/main/java/error/pirate/backend/productionReceiving/command/application/service/ProductionReceivingService.java
+++ SCM/backend/src/main/java/error/pirate/backend/productionReceiving/command/application/service/ProductionReceivingService.java
@@ -1,14 +1,37 @@
 package error.pirate.backend.productionReceiving.command.application.service;
 
 import error.pirate.backend.productionReceiving.command.application.dto.ProductionReceivingCreateRequest;
+import error.pirate.backend.productionReceiving.command.domain.aggregate.entity.ProductionReceiving;
+import error.pirate.backend.productionReceiving.command.domain.repository.ProductionReceivingRepository;
+import error.pirate.backend.user.command.domain.aggregate.entity.User;
+import error.pirate.backend.user.command.domain.repository.UserRepository;
+import error.pirate.backend.warehouse.command.domain.aggregate.entity.Warehouse;
+import error.pirate.backend.warehouse.command.domain.repository.WarehouseRepository;
+import error.pirate.backend.workOrder.command.domain.aggregate.entity.WorkOrder;
+import error.pirate.backend.workOrder.command.domain.repository.WorkOrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductionReceivingService {
 
+    private final ProductionReceivingRepository productionReceivingRepository;
+    private final WarehouseRepository warehouseRepository;
+    private final UserRepository userRepository;
+    private final WorkOrderRepository workOrderRepository;
+
+    @Transactional
     public void createProductionReceiving(ProductionReceivingCreateRequest request) {
 
+        Warehouse productionWarehouse = warehouseRepository.findById(request.getProductionWarehouseSeq()).orElseThrow();
+        Warehouse storeWarehouse = warehouseRepository.findById(request.getStoreWarehouseSeq()).orElseThrow();
+        User user = userRepository.findById(request.getUserSeq()).orElseThrow();
+        WorkOrder workOrder = workOrderRepository.findById(request.getWorkOrderSeq()).orElseThrow();
+
+        ProductionReceiving productionReceiving = ProductionReceiving.createProductionReceiving(productionWarehouse, storeWarehouse, user, workOrder, request);
+
+        productionReceivingRepository.save(productionReceiving);
     }
 }
