03116e377b7fa071d9dc547f6b8dbdbc7c264e47
beanteacher <dh65432@naver.com>
2024-12-09 18:20:38 +0900

    feat: 생산입고 완료 구현 (90/100) 진행중 ...

---
+++ SCM/backend/src/main/java/error/pirate/backend/salesOrder/query/repository/impl/SalesOrderQueryRepositoryImpl.java
@@ -0,0 +1,35 @@
+package error.pirate.backend.salesOrder.query.repository.impl;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import error.pirate.backend.productionReceiving.command.domain.aggregate.entity.ProductionReceiving;
+import error.pirate.backend.productionReceiving.command.domain.aggregate.entity.ProductionReceivingStatus;
+import error.pirate.backend.productionReceiving.query.dto.ProductionReceivingListDTO;
+import error.pirate.backend.salesOrder.command.domain.aggregate.entity.SalesOrder;
+import error.pirate.backend.salesOrder.query.dto.SalesOrderDTO;
+import error.pirate.backend.salesOrder.query.repository.SalesOrderQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static error.pirate.backend.productionReceiving.command.domain.aggregate.entity.QProductionReceiving.productionReceiving;
+import static error.pirate.backend.salesOrder.command.domain.aggregate.entity.QSalesOrder.salesOrder;
+import static error.pirate.backend.workOrder.command.domain.aggregate.entity.QWorkOrder.workOrder;
+
+@Repository
+@RequiredArgsConstructor
+public class SalesOrderQueryRepositoryImpl implements SalesOrderQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public SalesOrder findByProductionReceivingSeq(Long productionReceivingSeq) {
+        return queryFactory
+                .selectFrom(salesOrder)
+                .leftJoin(salesOrder).on(workOrder.salesOrder.salesOrderSeq.eq(salesOrder.salesOrderSeq))
+                .leftJoin(workOrder).on(productionReceiving.workOrder.workOrderSeq.eq(workOrder.workOrderSeq))
+                .where(productionReceiving.productionReceivingSeq.eq(productionReceivingSeq))
+                .fetchOne();
+    }
+}
