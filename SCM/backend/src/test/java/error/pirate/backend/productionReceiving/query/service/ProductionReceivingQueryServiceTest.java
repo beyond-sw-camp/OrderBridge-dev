7f9a32e6503a960edaa22504e579fc6882b759b1
beanteacher <dh65432@naver.com>
2024-12-09 12:24:07 +0900

    test: 생산입고 상세조회 테스트 코드 구현

--- SCM/backend/src/test/java/error/pirate/backend/productionReceiving/query/service/ProductionReceivingQueryServiceTest.java
+++ SCM/backend/src/test/java/error/pirate/backend/productionReceiving/query/service/ProductionReceivingQueryServiceTest.java
@@ -1,10 +1,8 @@
 package error.pirate.backend.productionReceiving.query.service;
 
-import error.pirate.backend.productionReceiving.command.application.service.ProductionReceivingService;
 import error.pirate.backend.productionReceiving.command.domain.aggregate.entity.ProductionReceivingStatus;
 import error.pirate.backend.productionReceiving.query.dto.ProductionReceivingListRequest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,7 +14,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,29 +26,45 @@ class ProductionReceivingQueryServiceTest {
     @Autowired
     ProductionReceivingQueryService productionReceivingQueryService;
 
-    private static Stream<Arguments> readProductionReceivingParam() {
+    private static Stream<Arguments> readProductionReceivingListParam() {
         return Stream.of(
-                arguments(1L, "2024-12-09", "2024-12-12", "2024/12/09-1", "BEFORE", 0, 10),
-                arguments(1L, null, null, "2024/12/09-1", "BEFORE", 0, 4),
-                arguments(1L, null, "2024-12-12", "2024/12/09-1", "BEFORE", 0, 8),
-                arguments(1L, "2024-12-09", "2024-12-12", null, "BEFORE", 0, 5),
-                arguments(1L, "2024-12-09", "2024-12-12", "2024/12/09-1", null, 0, 1),
-                arguments(1L, null, null, null, null, 0, 20)
+                arguments("2024-12-09", "2024-12-12", "2024/12/09-1", "BEFORE", 0, 10),
+                arguments(null, null, "2024/12/09-1", "BEFORE", 0, 4),
+                arguments(null, "2024-12-12", "2024/12/09-1", "BEFORE", 0, 8),
+                arguments("2024-12-09", "2024-12-12", null, "BEFORE", 0, 5),
+                arguments("2024-12-09", "2024-12-12", "2024/12/09-1", null, 0, 1),
+                arguments(null, null, null, null, 0, 20)
         );
     }
 
     @DisplayName("생산입고 리스트 조회")
     @ParameterizedTest(autoCloseArguments = true)
-    @MethodSource("readProductionReceivingParam")
-    void readProductionReceiving(Long userSeq, LocalDate searchStartDate,
+    @MethodSource("readProductionReceivingListParam")
+    void readProductionReceivingList(LocalDate searchStartDate,
                                  LocalDate searchEndDate, String searchName,
                                  ProductionReceivingStatus searchStatus,
                                  int offset, int limit) {
         ProductionReceivingListRequest request = new ProductionReceivingListRequest(
-            userSeq, searchStartDate, searchEndDate, searchName, searchStatus
+            searchStartDate, searchEndDate, searchName, searchStatus
         );
 
         Pageable pageable = PageRequest.of(offset, limit);
         assertDoesNotThrow(() -> productionReceivingQueryService.readProductionReceivingList(request, pageable));
+    }
+
+    private static Stream<Arguments> readProductionReceivingParam() {
+        return Stream.of(
+                arguments(1L),
+                arguments(2L),
+                arguments(3L)
+        );
+    }
+
+    @DisplayName("생산입고 상세 조회")
+    @ParameterizedTest(autoCloseArguments = true)
+    @MethodSource("readProductionReceivingParam")
+    void readProductionReceiving(Long productionReceivingSeq) {
+
+        assertDoesNotThrow(() -> productionReceivingQueryService.readProductionReceiving(productionReceivingSeq));
     }
 }
