fa5c6b644492992f56cd3095b0737b8274034caa
dispear <kkm7hjh@naver.com>
2024-12-09 14:08:55 +0900

    feat: 출하지시서 등록 거래처명 요청 제거

--- SCM/backend/src/main/java/error/pirate/backend/shippingInstruction/command/application/dto/ShippingInstructionItemDTO.java
+++ SCM/backend/src/main/java/error/pirate/backend/shippingInstruction/command/application/dto/ShippingInstructionItemDTO.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Schema(description = "출하지시서 품목 DTO")
 public class ShippingInstructionItemDTO {
-    private long itemSeq;    // 품목명
+    private long itemSeq;    // 품목 시퀀스
     private int shippingInstructionItemQuantity;    // 물품 수량
     private String shippingInstructionItemNote;     // 물품 비고
 }
