e3d4d0a1e0f236bac064d240616404d48271ec29
beanteacher <dh65432@naver.com>
2024-12-10 16:32:34 +0900

    feat: cors PATCH 추가

--- SCM/backend/src/main/java/error/pirate/backend/config/WebConfig.java
+++ SCM/backend/src/main/java/error/pirate/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOrigins("http://localhost:5173") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 자격 증명(쿠키 등) 허용
     }
