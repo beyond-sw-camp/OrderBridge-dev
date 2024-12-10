29b7a1be3d0828190ef98359a2fba4523abc82fa
sung-mini <sungmini9755@naver.com>
2024-12-11 05:59:32 +0900

    test: 거래처 수정 테스트 코드 작성

--- SCM/backend/src/test/java/error/pirate/backend/client/command/application/service/ClientServiceTest.java
+++ SCM/backend/src/test/java/error/pirate/backend/client/command/application/service/ClientServiceTest.java
@@ -2,6 +2,7 @@ package error.pirate.backend.client.command.application.service;
 
 import error.pirate.backend.BackendApplication;
 import error.pirate.backend.client.command.application.dto.ClientCreateRequest;
+import error.pirate.backend.client.command.application.dto.ClientUpdateRequest;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,7 +10,9 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -28,11 +31,25 @@ class ClientServiceTest {
         );
     }
 
+    private static Stream<Arguments> updateClientArguments() {
+        return Stream.of(
+                arguments(1L, new ClientUpdateRequest("Updated Client 1", "010-1111-2222", "updated1@test.com", "123-45-67890")),
+                arguments(2L, new ClientUpdateRequest("Updated Client 2", "010-2222-3333", "updated2@test.com", "987-65-43210")),
+                arguments(3L, new ClientUpdateRequest("Updated Client 3", "010-3333-4444", "updated3@test.com", "555-55-55555"))
+        );
+    }
+
     @DisplayName("거래처 등록 테스트 - 성공")
     @ParameterizedTest
     @MethodSource("createClientArguments")
     void createClientTest(ClientCreateRequest request) {
         assertDoesNotThrow(() -> clientService.createClient(request));
     }
-}
 
+    @DisplayName("거래처 수정 테스트 - 성공")
+    @ParameterizedTest
+    @MethodSource("updateClientArguments")
+    void updateClientTest(Long clientSeq, ClientUpdateRequest request) {
+        assertDoesNotThrow(() -> clientService.updateClient(clientSeq, request));
+    }
+}
