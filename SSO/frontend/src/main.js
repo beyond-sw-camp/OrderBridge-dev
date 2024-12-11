d091343534867d6947d2a911bdd2a1ab3bbd2535
beanteacher <dh65432@naver.com>
2024-12-11 15:28:28 +0900

    feat: 로그인 프로젝트 프론트엔드 npm install 추가

--- SSO/frontend/src/main.js
+++ SSO/frontend/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from '@/router'; // Vue Router
+import BootstrapVue3 from "bootstrap-vue-3";
+import {createPinia} from "pinia";
+
+const pinia = createPinia();
+
+const app = createApp(App);
+
+app.use(router); // Vue Router 사용
+app.use(BootstrapVue3);
+app.use(pinia);
+app.mount('#app')
