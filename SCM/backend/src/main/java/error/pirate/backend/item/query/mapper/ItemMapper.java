7e82e6ed06de05f195990d463f14880f2a22c401
sung-mini <sungmini9755@naver.com>
2024-12-19 03:55:20 +0900

    refactor: 품목 조회 기능 추가 및 수정

--- SCM/backend/src/main/java/error/pirate/backend/item/query/mapper/ItemMapper.java
+++ SCM/backend/src/main/java/error/pirate/backend/item/query/mapper/ItemMapper.java
@@ -11,11 +11,17 @@ public interface ItemMapper {
 
     List<ItemResponse> findItemListByFilter(
             @Param("itemName") String itemName,
-            @Param("itemDivision") String itemDivision,
-            @Param("itemExpirationHour") Integer itemExpirationHour,
-            @Param("sortBy") String sortBy,
-            @Param("sortDirection") String sortDirection,
-            @Param("offset") int offset,
-            @Param("size") int size
+            @Param("itemDivisions") List<String> itemDivisions,
+            @Param("minExpirationHour") Integer minExpirationHour,
+            @Param("maxExpirationHour") Integer maxExpirationHour,
+            @Param("size") Integer size,
+            @Param("offset") Integer offset
+    );
+
+    int countItemsByFilter(
+            @Param("itemName") String itemName,
+            @Param("itemDivisions") List<String> itemDivisions,
+            @Param("minExpirationHour") Integer minExpirationHour,
+            @Param("maxExpirationHour") Integer maxExpirationHour
     );
 }
