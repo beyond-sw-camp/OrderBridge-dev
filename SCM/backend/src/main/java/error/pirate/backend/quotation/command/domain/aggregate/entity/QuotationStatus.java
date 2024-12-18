e3d881a7530ef6d17a15c91ffff345b7f141af43
Huichan Bak <hc.bak@outlook.com>
2024-12-18 11:43:00 +0900

    feat: 견적서 상태 분류 조회 API 추가

--- SCM/backend/src/main/java/error/pirate/backend/quotation/command/domain/aggregate/entity/QuotationStatus.java
+++ SCM/backend/src/main/java/error/pirate/backend/quotation/command/domain/aggregate/entity/QuotationStatus.java
@@ -1,9 +1,34 @@
 package error.pirate.backend.quotation.command.domain.aggregate.entity;
 
-public enum QuotationStatus {
-    BEFORE, // 결재전
-    AFTER,  // 결재후
-    REFUSAL, // 반려
-    DELETE  // 삭제
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public enum QuotationStatus {
+    BEFORE("결재 전"),
+    AFTER("결재 후"),
+    REFUSAL("반려"),
+    DELETE("삭제");
+
+    private final String value;
+
+    @Getter
+    public static class QuotationStatusResponse {
+        private final String key;
+        private final String value;
+
+        public QuotationStatusResponse(String key, QuotationStatus quotationStatus) {
+            this.key = key;
+            this.value = quotationStatus.getValue();
+        }
+    }
+
+    public static List<QuotationStatusResponse> readQuotationStatusList() {
+        return Arrays.stream(QuotationStatus.class.getEnumConstants()).map(key ->
+                new QuotationStatusResponse(key.toString(), QuotationStatus.valueOf(key.toString()))).toList();
+    }
 }
