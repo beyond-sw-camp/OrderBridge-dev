1f383d1f645e5b749a81c64387e9ff20941c4235
beanteacher <dh65432@naver.com>
2024-12-20 09:18:34 +0900

    bug: 충돌 해결

--- SCM/backend/src/main/java/error/pirate/backend/item/query/service/ItemQueryService.java
+++ SCM/backend/src/main/java/error/pirate/backend/item/query/service/ItemQueryService.java
@@ -22,10 +22,8 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
