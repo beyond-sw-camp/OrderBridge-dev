86c1b049800ae8480923b5e6572f8a92f587f8c5
AYeong-Jeon <ay98923@naver.com>
2024-12-23 21:46:47 +0900

    feat: 주문서 제거

--- SCM/frontend/src/components/purchaseOrder/PurchaseOrderInputForm.vue
+++ SCM/frontend/src/components/purchaseOrder/PurchaseOrderInputForm.vue
@@ -7,8 +7,8 @@ import router from "@/router/index.js";
 
 const pageSize = ref(10);
 const pageNumber = ref(1);
-const salesOrderList = ref([]);
 const purchaseItemList = ref([]);
+
 const purchaseOrderItemList = ref([]);
 const totalCount = ref(null);
 
@@ -23,33 +23,21 @@ const formData = ref({
   purchaseOrderItemList: {}
 });
 
-const fetchSalesOrderList = async () => {
+const fetchItems = async () => {
   try {
-    const response = await axios.get(`sales-order`, {
-      params: {
-        startDate: null,
-        endDate: null,
-        warehouseName: null,
-        workOrderStatus: 'ONGOING',
-        page: pageNumber.value,
-        size: pageSize.value
-      }, paramsSerializer: (params) => {
+    const response = axios.get(`item`, {
+      paramsSerializer: (salesOrderSeq) => {
         const filteredParams = Object.fromEntries(
-            Object.entries(params).filter(([_, value]) => value !== null && value !== undefined)
+            Object.entries(salesOrderSeq).filter(([_, value]) => value !== null && value !== undefined)
         );
         return new URLSearchParams(filteredParams).toString();
       }
     });
 
-    salesOrderList.value = response.data.salesOrder;
-    // 초기화: 모든 작업 지시서의 체크박스를 false로 설정
-    salesOrderList.value.forEach((workOrder) => {
-      checkSalesOrderList.value[workOrder.salesOrderSeq] = false;
-    });
-    totalCount.value = response.data.totalSalesOrder;
-
+    purchaseItemList.value = Array.isArray(response.data.items) ? response.data.items : [];
+    openItemModal();
   } catch (error) {
-    console.error("주문서 불러오기 실패 :", error);
+    console.error("Failed to fetch items:", error);
   }
 };
 
@@ -70,86 +58,12 @@ const formatStatus = (status) => {
   return status; // 상태가 다른 경우 그대로 반환
 };
 
-// 페이지 이동
-const salesOrderListHandlePage = (pageNumber) => {
-  pageNumber.value = Number(pageNumber.value);
-  fetchSalesOrderList();
-};
-
 onMounted(() => {
   console.log()
-  fetchSalesOrderList();
+  fetchItems();
 });
 
-const checkSalesOrderItem = ref([]);
-const fetchSalesOrder = async (salesOrderSeq) => {
-  try {
-    const response = await axios.get(`sales-order/${salesOrderSeq}`, {
-      paramsSerializer: (salesOrderSeq) => {
-        const filteredParams = Object.fromEntries(
-            Object.entries(salesOrderSeq).filter(([_, value]) => value !== null && value !== undefined)
-        );
-        return new URLSearchParams(filteredParams).toString();
-      }
-    });
-
-    const itemList = response.data.salesOrderItem || [];
-    itemList.forEach((item) => {
-      const checkSalesOrder = {
-        itemSeq: response.data.salesOrderItem.itemSeq,
-        itemImageUrl: response.data.salesOrderItem.itemImageUrl, // 품목 이미지
-        itemName: response.data.salesOrderItem.itemName, // 품목명
-        salesOrderItemPrice: response.data.salesOrderItem.salesOrderItemPrice, // 품목 가격
-        salesOrderItemQuantity: response.data.salesOrderItem.salesOrderItemQuantity, // 품목 단위
-        salesOrderItemNote: response.data.salesOrderItem.salesOrderItemNote
-      }
-
-      formData.value.purchaseOrderItemList.push({
-        itemSeq: checkSalesOrder.itemSeq,
-        itemName: checkSalesOrder.itemName,
-        salesOrderItemQuantity: checkSalesOrder.salesOrderItemQuantity,
-        salesOrderItemPrice: checkSalesOrder.salesOrderItemPrice,
-        salesOrderItemNote: checkSalesOrder.salesOrderItemNote
-      });
-      formData.value.purchaseOrderExtendedPrice += checkSalesOrder.salesOrderItemPrice;
-      checkSalesOrderItem.value.push(checkSalesOrder);
-    });
-
-  } catch (error) {
-    console.error("주문서 불러오기 실패 :", error);
-  }
-};
-
-function openOrderModal() {
-  const modal = document.getElementById('workOrderModal');
-  modal.removeAttribute('aria-hidden'); // 모달 활성화 시 aria-hidden 제거
-  modal.style.display = 'block';
-  modal.focus(); // 포커스를 모달로 이동
-}
-
-function closeOrderModal() {
-  const modal = document.getElementById('workOrderModal');
-  modal.setAttribute('aria-hidden', 'true'); // 모달 비활성화 시 aria-hidden 추가
-  modal.style.display = 'none';
-}
-
 function openItemModal() {
-  try {
-    const response = axios.get(`item`, {
-      paramsSerializer: (salesOrderSeq) => {
-        const filteredParams = Object.fromEntries(
-            Object.entries(salesOrderSeq).filter(([_, value]) => value !== null && value !== undefined)
-        );
-        return new URLSearchParams(filteredParams).toString();
-      }
-    });
-
-    purchaseItemList.value = response.data;
-
-  } catch (error) {
-    console.error("품목 불러오기 실패 :", error);
-  }
-
   const modal = document.getElementById('workItemModal');
   modal.removeAttribute('aria-hidden'); // 모달 활성화 시 aria-hidden 제거
   modal.style.display = 'block';
@@ -164,33 +78,26 @@ function closeItemModal() {
 
 const removeItem = (salesOrderSeq) => {
   purchaseOrderItemList.value = purchaseOrderItemList.value.filter(
-      (item) => item.itemSeq !== itemSeq
+      (item) => item.itemSeq !== salesOrderSeq
   );
 
-  delete checkSalesOrderList.value[itemSeq];
+  delete purchaseOrderItemList.value[itemSeq];
 };
 
-const addToOrderList = async (selectedOrder) => {
-  await fetchSalesOrder(selectedOrder.salesOrderSeq);
-
+const addToOrderList = (selectedOrder) => {
   const existingItem = purchaseOrderItemList.value.find(
-      (item) => item.salesOrderSeq === selectedOrder.salesOrderSeq
+      (item) => item.itemSeq === selectedOrder.itemSeq
   );
-
   if (!existingItem) {
     purchaseOrderItemList.value.push({
-      salesOrderSeq: selectedOrder.salesOrderSeq,
       itemName: selectedOrder.itemName,
-      itemImageUrl: selectedOrder.itemImageUrl || '', // 이미지 URL이 없을 경우 기본값 처리
       salesOrderItemPrice: selectedOrder.salesOrderItemPrice || 0,
       salesOrderItemQuantity: selectedOrder.salesOrderItemQuantity || 0,
-      salesOrderItemNote: selectedOrder.salesOrderItemNote || '',
+      salesOrderItemNote: selectedOrder.salesOrderItemNote || ""
     });
   } else {
-    alert('이미 추가된 주문서입니다.');
+    alert("This order has already been added.");
   }
-
-  closeOrderModal();
   closeItemModal();
 };
 
@@ -257,9 +164,9 @@ const createPurchaseOrder = async () => {
 <div class="d-flex justify-content-center">
     <div class="col-md-10 d-flex flex-column">
       <h5 class="px-4">발주서 품목
-        <span class="w-75" size="sm" @click="openOrderModal" data-bs-toggle="modal" data-bs-target="#openOrderModal">
+<!--        <span class="w-75" size="sm" @click="openOrderModal" data-bs-toggle="modal" data-bs-target="#openOrderModal">
         <b-button variant="light" size="sm" class="button ms-2" style="float: right">주문서 불러오기</b-button>
-        </span>
+        </span>-->
       </h5>
       <div v-for="purchaseOrder in purchaseOrderItemList" :key="purchaseOrder.itemSeq" class="mx-5 my-3">
         <div class="d-flex flex-row border border-secondary rounded p-3 position-relative">
@@ -298,44 +205,6 @@ const createPurchaseOrder = async () => {
         <b-button v-if="props" @click="updatePurchaseOrder" variant="light" size="sm" class="button ms-2">수정</b-button>
         <b-button v-else @click="createPurchaseOrder" variant="light" size="sm" class="button ms-2">등록</b-button>
 
-      </div>
-    </div>
-  </div>
-
-  <!-- Modal -->
-  <div class="modal fade" id="openOrderModal" tabindex="-1" aria-labelledby="workOrderModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5">주문서 선택</h1>
-          <div class="ms-5">검색결과: {{ totalCount }}개</div>
-          <button type="button" @click="closeOrderModal" class="button btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div style="max-height: 500px; overflow-y: scroll" class="d-flex row justify-content-center align-items-center">
-
-            <div class="list-headline row">
-              <div class="list-head col-5">주문서명</div>
-              <div class="list-head col-2">거래처명</div>
-              <div class="list-head col-3">물품 납기일</div>
-              <div class="list-head col-2">상태</div>
-            </div>
-            <template v-if="salesOrderList.length > 0">
-              <div v-for="salesOrder in salesOrderList" :key="salesOrder.salesOrderSeq" class="list-line row" @click="addToOrderList(salesOrder)">
-                <div class="list-body col-5 left">{{ salesOrder.salesOrderName }}</div>
-                <div class="list-body col-2">{{ salesOrder.clientName }}</div>
-                <div class="list-body col-3">{{ dayjs(salesOrder.salesOrderOrderDate).format('YYYY/MM/DD') }}</div>
-                <div class="list-body col-2">{{ formatStatus(salesOrder.salesOrderStatus) }}</div>
-              </div>
-            </template>
-            <template v-else>
-              <b-card-text class="no-list-text">해당 검색조건에 부합한 주문서가 존재하지 않습니다.</b-card-text>
-            </template>
-          </div>
-        </div>
-        <div class="modal-footer pagination">
-          <b-pagination v-model="pageNumber" :totalRows="totalCount" :perPage="pageSize"/>
-        </div>
       </div>
     </div>
   </div>
