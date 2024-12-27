9dd88c81903f688a742c0089f3341361bad5b24f
beanteacher <dh65432@naver.com>
2024-12-27 12:26:19 +0900

    refactor: Swagger RefreshToken 넣을 수 있도록 수정

--- SCM/backend/src/main/java/error/pirate/backend/security/JwtAccessDeniedHandler.java
+++ SCM/backend/src/main/java/error/pirate/backend/security/JwtAccessDeniedHandler.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
 
 /* 인가 실패 시 응답 설정 */
+@Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException, IOException {
+        log.info("JwtAccessDeniedHandler : {}", accessDeniedException.toString());
         /* 리소스에 접근 권한이 없는데 접근하여 인가 되지 않은 경우 => 403 오류 */
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json;charset=UTF-8");
