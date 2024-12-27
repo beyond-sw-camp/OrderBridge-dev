a841e0f24757be3ff60513784c762d221d94b192
beanteacher <dh65432@naver.com>
2024-12-27 15:35:35 +0900

    feat: Github Actions 테스트 완료

--- SCM/backend/src/main/java/error/pirate/backend/config/AppConfig.java
+++ SCM/backend/src/main/java/error/pirate/backend/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
-    @Beansetestsetests
+    @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
